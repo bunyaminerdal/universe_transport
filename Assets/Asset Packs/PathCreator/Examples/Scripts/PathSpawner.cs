c9c284cc2f541adb5eb7d9cafe8fe26594f333ac
leonidas131 <leonidas131@hotmail.com>
2021-06-28 15:05:48 +0300

    first commit
    start with generating universe

---
+++ Assets/Asset Packs/PathCreator/Examples/Scripts/PathSpawner.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PathCreation;
+using UnityEngine;
+
+namespace PathCreation.Examples
+{
+
+    public class PathSpawner : MonoBehaviour
+    {
+        [SerializeField]
+        private PathCreator pathPrefab;
+        [SerializeField]
+        private PathFollower planetPrefab;
+        [SerializeField]
+        private GameObject sunPrefab;
+
+        private GameObject spawnPoint;
+        private Transform[] spawnPoints;
+        private float planetDistance = 2f;
+        void Start()
+        {
+
+            int planetCount = Random.Range(3, 6);
+            spawnPoints = new Transform[planetCount];
+            Debug.Log(planetCount);
+            var sun = Instantiate(sunPrefab, transform);
+            for (int i = 1; i < planetCount + 1; i++)
+            {
+                spawnPoint = new GameObject();
+                spawnPoint.transform.parent = transform;
+                spawnPoint.transform.localScale = new Vector3(i * planetDistance, i * planetDistance, i * planetDistance);
+                spawnPoints[i - 1] = spawnPoint.transform;
+            }
+
+            foreach (Transform t in spawnPoints)
+            {
+                var path = Instantiate(pathPrefab, t.position, t.rotation);
+                var follower = Instantiate(planetPrefab);
+                follower.pathCreator = path;
+                follower.speed = 1f / t.transform.localScale.x;
+                path.transform.parent = t;
+                follower.transform.parent = t;
+            }
+        }
+    }
+
+}
