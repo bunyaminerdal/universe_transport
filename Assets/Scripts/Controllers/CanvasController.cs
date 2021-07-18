f8f6b51207d8303c72a5b5111c61f8527f3f683c
leonidas131 <leonidas131@hotmail.com>
2021-07-18 22:30:47 +0300

    game time system

--- Assets/Scripts/Controllers/CanvasController.cs
+++ Assets/Scripts/Controllers/CanvasController.cs
@@ -1,15 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 using TMPro;
+using System.Globalization;
 
 public class CanvasController : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text pauseText;
+
+    [Header("date time text")]
+    [SerializeField]
+    private TMP_Text yearText;
+    [SerializeField]
+    private TMP_Text monthText;
+    [SerializeField]
+    private TMP_Text dayText;
+
     private void OnEnable()
     {
         UIEventHandler.PauseTextClicked.AddListener(PauseTextClicked);
+        UIEventHandler.YearChanged.AddListener(OnYearChanged);
+        UIEventHandler.MonthChanged.AddListener(OnMonthChanged);
+        UIEventHandler.DayChanged.AddListener(OnDayChanged);
     }
     void Start()
     {
@@ -24,6 +36,9 @@ public class CanvasController : MonoBehaviour
     private void OnDisable()
     {
         UIEventHandler.PauseTextClicked.RemoveListener(PauseTextClicked);
+        UIEventHandler.YearChanged.RemoveListener(OnYearChanged);
+        UIEventHandler.MonthChanged.RemoveListener(OnMonthChanged);
+        UIEventHandler.DayChanged.RemoveListener(OnDayChanged);
     }
     private void PauseTextClicked(float time)
     {
@@ -32,4 +47,18 @@ public class CanvasController : MonoBehaviour
         else
             pauseText.enabled = false;
     }
+
+    private void OnYearChanged(int year)
+    {
+        yearText.text = year.ToString();
+    }
+    private void OnMonthChanged(int month)
+    {
+        monthText.text = CultureInfo.CurrentCulture.DateTimeFormat.GetMonthName(month);
+    }
+    private void OnDayChanged(int day)
+    {
+        dayText.text = day.ToString();
+    }
+
 }
---
+++ Assets/TimeController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeController : MonoBehaviour
+{
+    [SerializeField]
+    private int startYear;
+    [SerializeField]
+    private int startMonth;
+    [SerializeField]
+    private int startDay;
+    [SerializeField]
+    private int startHour;
+    [SerializeField]
+    private float startMinute;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UIEventHandler.DayChanged?.Invoke(startDay);
+        UIEventHandler.MonthChanged?.Invoke(startMonth);
+        UIEventHandler.YearChanged?.Invoke(startYear);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.deltaTime > 0)
+        {
+            startMinute += 1 * Time.timeScale;
+            if (startMinute > 59)
+            {
+                startMinute = 0;
+                startHour += 1;
+            }
+
+            if (startHour > 23)
+            {
+                startHour = 0;
+                startDay += 1;
+                UIEventHandler.DayChanged?.Invoke(startDay);
+            }
+            if (startDay > DateTime.DaysInMonth(startYear, startMonth))
+            {
+                startDay = 1;
+                UIEventHandler.DayChanged?.Invoke(startDay);
+                startMonth += 1;
+                UIEventHandler.MonthChanged?.Invoke(startMonth);
+            }
+            if (startMonth > 11)
+            {
+                startMonth = 1;
+                UIEventHandler.MonthChanged?.Invoke(startMonth);
+                startYear += 1;
+                UIEventHandler.YearChanged?.Invoke(startYear);
+            }
+        }
+    }
+
+    private void setTime(int year,
+                         int month,
+                         int day
+                         )
+    {
+        startYear = year;
+        startMonth = month;
+        startDay = day;
+        UIEventHandler.DayChanged?.Invoke(startDay);
+        UIEventHandler.MonthChanged?.Invoke(startMonth);
+        UIEventHandler.YearChanged?.Invoke(startYear);
+    }
+}
